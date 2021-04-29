a8c4ba0773d6890afbfdde9820823d13f8e2f213
Atsushi <atsushithesushi@gmail.com>
2021-04-29 09:27:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const Discord = require("discord.js");
+const {
+    Discord
+} = require("discord.js");
 const {
     response
 } = require("express");
@@ -7,13 +9,14 @@ const {
 } = require("./config.js")
 
 const webhookClient = new Discord.WebhookClient(config.webhookId, config.webhookToken);
-const express = require("express");
-const app = express();
+const express       = require("express");
+const app           = express();
 
 app.use(express.static("public"));
-app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/index.html");
-    webhookClient.send("Olá, um usuário acessou o website.")
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+    webhookClient.send("A user has accessed the website.")
 });
 
 const listener = app.listen(process.env.PORT, () => {
